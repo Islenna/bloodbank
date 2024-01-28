4111d53ac7727b86a5fc916af690bd9757144823
David Kral <dockral@gmail.com>
2024-01-27 21:26:29 -0800

    Mobile navbar fix

--- frontend/src/components/Header/CustomNavbar.jsx
+++ frontend/src/components/Header/CustomNavbar.jsx
@@ -7,6 +7,13 @@ import logo from '../../assets/logo.png';
 export default function CustomNavbar(props) {
     const navigate = useNavigate();
     const { userRole, logout } = useAuth();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+
+    const toggleMenu = () => {
+        console.log('Toggle menu clicked'); // Add this line
+        setIsMenuOpen(!isMenuOpen);
+    };
 
 
     const handleLogout = (e) => {
@@ -34,14 +41,76 @@ export default function CustomNavbar(props) {
                     type="button"
                     className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                     aria-controls="navbar-default"
-                    aria-expanded="false"
+                    aria-expanded={isMenuOpen}
+                    onClick={toggleMenu}
                 >
                     <span className="sr-only">Open main menu</span>
-                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+                    <svg
+                        className="w-5 h-5"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 17 14"
+                    >
+                        <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M1 1h15M1 7h15M1 13h15"
+                        />
                     </svg>
                 </button>
-                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+
+                {/* Mobile navigation menu */}
+                <div
+                    className={`${isMenuOpen ? 'block' : 'hidden'} md:hidden`}
+                    id="navbar-mobile"
+                >
+                    <ul className="font-medium p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50">
+                        {/* Common link for all users */}
+                        <li>
+                            <a href="/bloodfinder" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded">Get Blood</a>
+                        </li>
+
+                        {/* Conditionally render based on user's userRole */}
+                        {userRole === 'admin' && (
+                            <>
+                                <li>
+                                    <a href="/users" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded">User Management</a>
+                                </li>
+                                {/* Other admin-specific links */}
+                            </>
+                        )}
+
+                        {['admin', 'manager'].includes(userRole) && (
+                            <>
+                                <li>
+                                    <Link to="/inventory" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100">Inventory Management</Link>
+                                </li>
+                                {/* Other links for admin and manager */}
+                            </>
+                        )}
+
+                        {/* Common links for all roles */}
+                        <li>
+                            <Link to="/suggestions" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100">Suggestions</Link>
+                        </li>
+                        <li>
+                            <Link to={`/inventory/consumed`} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100">Analytics</Link>
+                        </li>
+                        <li>
+                            <a href="#" onClick={handleLogout} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100">Logout</a>
+                        </li>
+                    </ul>
+                </div>
+
+
+
+                <div
+                    className="hidden md:block"
+                    id="navbar-desktop"
+                >
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         {/* Common link for all users */}
                         <li>
