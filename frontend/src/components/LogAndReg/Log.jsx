98732a848c9cf1c5fe907ae778f431381d6be9e0
Islenna <dockral@gmail.com>
2024-02-26 08:50:26 -0800

    Log/Reg fixes

--- frontend/src/components/LogAndReg/Log.jsx
+++ frontend/src/components/LogAndReg/Log.jsx
@@ -9,6 +9,7 @@ function Log() {
     const navigate = useNavigate();
     const [loginEmail, setLoginEmail] = useState('');
     const [loginPassword, setLoginPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
     const loginHandler = (e) => {
         e.preventDefault();
@@ -28,6 +29,7 @@ function Log() {
             })
             .catch((err) => {
                 console.log(err);
+                setErrorMessage('Incorrect username or password');
             });
     }
 
@@ -58,6 +60,7 @@ function Log() {
                                     required />
                             </div>
                             <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                            {errorMessage && <p className="text-sm text-red-500">{errorMessage}</p>}
                             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                                 Don’t have an account yet? <Link to="/reg" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
                             </p>
--- frontend/src/components/LogAndReg/Reg.jsx
+++ frontend/src/components/LogAndReg/Reg.jsx
@@ -15,6 +15,7 @@ function Reg() {
 
     const registrationHandler = (e) => { //When copying Tailwind, change class to className and for to htmlFor.
         e.preventDefault();
+        setRegError('');
 
         if (regPassword !== confirmPassword) {
             setRegError('Passwords do not match');
@@ -89,6 +90,7 @@ function Reg() {
                                 </div>
                             </div>
                             <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create an account</button>
+                            {regError && <p className="text-sm text-red-500">{regError}</p>}
                             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                                 Already have an account? <a href="/" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
                             </p>
