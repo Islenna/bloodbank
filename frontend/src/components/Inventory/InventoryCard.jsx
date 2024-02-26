802242453a451849210c0eda5ffccc4ec37d40a2
Islenna <dockral@gmail.com>
2024-02-26 14:34:33 -0800

    Styling

--- frontend/src/components/Inventory/InventoryCard.jsx
+++ frontend/src/components/Inventory/InventoryCard.jsx
@@ -101,14 +101,14 @@ function InventoryCard() {
                 <button
                     type="button"
                     onClick={() => navigate(`/inventory/${id}/consume`)}
-                    className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                    className="mr-4 text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
                 >
                     Consume
                 </button>
                 <button
                     type="button"
                     onClick={() => changeOnHold()}
-                    className="text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-900"
+                    className="ml-4 text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-900"
                 >
                     {inventory.onHold ? 'Remove Hold' : 'Put On Hold'}
                 </button>
--- frontend/src/components/Inventory/InventoryEdit.jsx
+++ frontend/src/components/Inventory/InventoryEdit.jsx
@@ -162,7 +162,7 @@ export default function InventoryEditForm() {
                     </div>
                     <button
                         type="submit"
-                        className="text-white bg-blue-700 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-blue-600 dark:focus:ring-red-900"
+                        className="mr-4 text-white bg-blue-700 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-blue-600 dark:focus:ring-red-900"
                     >
                         Update Inventory
                     </button>
@@ -170,7 +170,7 @@ export default function InventoryEditForm() {
                         <button
                             type="submit"
                             onClick={handleDelete}
-                            className="text-white bg-red-700 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-red-600 dark:focus:ring-red-900"
+                            className="ml-4 text-white bg-red-700 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-red-600 dark:focus:ring-red-900"
                         >
                             Delete Inventory
                         </button>
